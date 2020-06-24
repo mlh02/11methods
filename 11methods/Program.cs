f074dcf3681a9a7b7ae62c2837f47b382eaaa4e2
Montather Handal <ali.louy2005@gmail.com>
2020-06-24 13:11:09 -0500

    Added +3 adder

--- 11methods/Program.cs
+++ 11methods/Program.cs
@@ -6,23 +6,40 @@ namespace _11methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter first Number Hint(1000)");
-            // get input
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter second number");
-            // get second input
-            int num2 = int.Parse(Console.ReadLine());
-            // invoke
-            NumPrint(num1, num2);
+            //Console.WriteLine("Enter first Number Hint(1000)");
+            //// get input
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter second number");
+            //// get second input
+            //int num2 = int.Parse(Console.ReadLine());
+            //// invok
+
+            Console.WriteLine("start 3 number");
+            int randomsStart = int.Parse(Console.ReadLine());
+            Console.WriteLine("end number");
+            int randomsEnd = int.Parse(Console.ReadLine());
+
+            //NumPrint(num1, num2);
+            NumByThrees(randomsStart, randomsEnd);
         }
         static void NumPrint(int num1, int num2)
         {
             //loop through given 2 numbers
-            for (int i = num1; i >= num2 ; i--)
+            for (int i = num1; i >= num2; i--)
             {
                 //print out numbers
                 Console.WriteLine(i);
             }
         }
-    }
+
+            static void NumByThrees(int num, int num2)
+            {
+                //loop through given 2 numbers
+                for (int i = num; i < num2; i = i + 3)
+                {
+                    //print out numbers
+                    Console.WriteLine(i);
+                }
+            }
+        }
 }
